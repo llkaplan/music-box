e1da744ae303520d98cbec7576e5f9c7b0c3c8a1
Joan Williams <joanlinw@gmail.com>
2021-02-07 20:11:47 -0800

    adding sound selector functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 // MUSIC-CREATION.HTML ///////////////////////////////
 
+// const { selectors } = require("sizzle");
+
 // Hover Image Change for the Sound Options Section
 
 $(function(){
@@ -69,3 +71,131 @@ $(function(){
     }
     });
   });
+
+
+// Take Select Value and Add to Box
+
+$( "#sendToBox" )
+    .change(function () {
+        var str = "";
+        $( "select option:selected" ).each(function() {
+        str += $( this ).text() + " ";
+        });
+        $( "#sendToBoxButton" ).text( "Send To " + str );
+    })
+.change();
+
+
+// Rotating Song Selector
+
+var songs = ["Super Mario 64", "Jingle Bells", "Star Wars", "Happy Birthday", "Clair de Lune"];
+var songDivs = ["#songDiv0", "#songDiv1", "#songDiv2", "#songDiv3", "#songDiv4"];
+var songImgCode = ['images/song_plug.png', 'images/song_bass.png', 'images/song_keys.png', 'images/song_party.png', 'images/song_bass.png'];
+var songSoundFile = ['sounds/clairDeLune.mp3', 'sounds/starWarsMainTheme.mp3', 'sounds/superMarioBros.mp3', 'sounds/starWarsMainTheme.mp3', 'sounds/superMarioBros.mp3'];
+
+
+function getSongName(song) {
+    if (song == songs[0]) {
+        console.log(songs[0]);
+        return songs[0];
+    } else if (song == songs[1]) {
+        console.log(songs[1]);
+        return songs[1];
+    } else if (song == songs[2]) {
+        console.log(songs[2]);
+        return songs[2];
+    } else if (song == songs[3]) {
+        console.log(songs[3]);
+        return songs[3];
+    } else if (song == songs[4]) {
+        console.log(songs[4]);
+        return songs[4];
+    }
+}
+
+getSongName(songs[0]);
+
+function getSongImage(song) {
+    if (song === songs[0]) {
+        console.log(song);
+        return songImgCode[0];
+    } else if  (song === songs[1]) {
+        return songImgCode[1];
+    } else if  (song === songs[2]) {
+        console.log(song);
+        return songImgCode[2];
+    } else if  (song === songs[3]) {
+        return songImgCode[3];
+    } else if  (song === songs[4]) {
+        return songImgCode[4];
+    }
+}
+
+function createARegularSongDiv(song, divNumber) {
+    $( divNumber ).empty();
+    $( divNumber ).append( "<img src='" + getSongImage(song) + "'>" );
+    $( divNumber ).append( "<p>" + getSongName(song) + "</p>" );
+}
+
+function createMiddleSongDiv(song) {
+    $( songDiv2 ).empty();
+    $( songDiv2 ).append( "<img src='" + getSongImage(song) + "'>" );
+    $( songDiv2 ).append( "<p class='bold'>" + getSongName(song) + "</p>" );
+}
+
+function updateSongInPlayer(songSoundFile) {
+    $( "#audioPlayer" ).attr("src", songSoundFile);
+}
+
+
+// createARegularSongDiv(songs[0], songDivs[0]);
+// createARegularSongDiv(songs[2], songDivs[1]);
+// createARegularSongDiv(songs[4], songDivs[3]);
+
+// createMiddleSongDiv(songs[4]);
+
+    // var theDiv = $("#songDivMain");
+    // var classes = "songsUnselected center";
+    // var textClass = "";
+
+    // for (i = 0; i < songs.length; ++i) {
+    //     classes = "songSelected center flexCenter";
+    //     textClass = "bold largerText";
+    // }
+
+    // var songDiv = $('<div></div>');
+    // songDiv.addClass(classes);
+    // var innerDiv = $('<div></div>');
+    // innerDiv.append( getSongImage(song) );
+    // innerDiv.append("<p>" + getSongName(song) + "</p>");
+
+
+
+
+
+// $( ".container" ).append( $( "h2" ) );
+
+//     var thediv = $(“#songDivMain”); // get the parent div
+//     thediv.empty(); // remove all children
+//     var classes = "songsUnselected center"; // default classes
+//     var textClass = "";
+  
+//     for (int i=0; i<songs.length; ++i) {
+//       var song = songs[i];
+//       if (i === 2) {
+//           classes = "songSelected center flexCenter";
+//           textClass = "bold largerText";
+//       }
+      
+//       // Create a div
+//       var songDiv = $('<div></div>');
+//       songDiv.appendClass(classes);
+//       var innerDiv = $('<div></div>');
+//       innerDiv.append("<img src=" + getSongImage(song) + "/>");
+//       innerDiv.append("<p
+  
+  
+//     }
+  
+//   }
+
